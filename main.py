9503edf30d99334bb697e3ef3c98862e4edc9193
hikmat <hikmat.saud3@gmail.com>
2026-09-13 21:31:20 +0400

    agents work is done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-from src.tools.tools import web_search,scrape_url
+from src.pipeline.pipeline import run_research_pipeline
 
-print(scrape_url("https://reddit.com/r/artificial"))
+
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
